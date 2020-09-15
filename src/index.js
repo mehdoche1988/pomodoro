0b9cc0fdcbbd814d9c5733ab36c100f2bc0a8c6e
mehdoche1988 <mehdi.semlali.1988@gmail.com>
2020-09-15 14:10:54 +0200

    litle chagement

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Break from './components/Break'
+import Session from "./components/Session"
+import TimeLeft from "./components/TimeLeft"
 
 ReactDOM.render(<App />, document.getElementById('app'))
